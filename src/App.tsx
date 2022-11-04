9792cd932deab31161a184e265227b703a71d7fb
Davin Wijaya <davinwijaya20@gmail.com>
2022-11-04 22:15:40 +0700

    admin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/Home';
 import LandingPage from './Components/LandingPage';
 import './App.css';
+import AdminPage from './Components/AdminPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />        
+        <Route path="/admin" element={<AdminPage />} />        
       </Routes>
     </BrowserRouter>
   );
